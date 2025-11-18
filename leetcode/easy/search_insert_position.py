33147b1cd2bec89b92bb77659e5e2d46cbd20abe
slitvack <slitvack@infinidat.com>
2025-11-19 00:39:32 +0200

    add search insert position challenge

---
+++ leetcode/easy/search_insert_position.py
@@ -0,0 +1,38 @@
+"""
+Given a sorted array of distinct integers and a target value, return the index if the target is found.
+If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+"""
+
+
+def search_insert_position(nums, target):
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            right -= 1
+        else:
+            left += 1
+    return left
+
+
+print(search_insert_position([1, 3, 5, 6], 7))
+print(search_insert_position([1, 3, 6, 7], 5))
+
