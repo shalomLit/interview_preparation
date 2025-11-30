84ed51a64e5aec419fbc3bb13afe02d8491eb351
slitvack <slitvack@infinidat.com>
2025-11-30 10:06:22 +0200

    add sum_odd_length_subarrays challenge

---
+++ leetcode/easy/sum_all_odd_subarrays.py
@@ -0,0 +1,33 @@
+"""
+Given an array of positive integers arr, return the sum of all possible odd-length subarrays of arr.
+A subarray is a contiguous subsequence of the array.
+
+Input: arr = [1,4,2,5,3]
+Output: 58
+
+Explanation: The odd-length subarrays of arr and their sums are:
+[1] = 1
+[4] = 4
+[2] = 2
+[5] = 5
+[3] = 3
+[1,4,2] = 7
+[4,2,5] = 11
+[2,5,3] = 10
+[1,4,2,5,3] = 15
+If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
+"""
+
+
+def sum_odd_length_subarrays(arr):
+    n = len(arr)
+    total = 0
+    for i in range(n):
+        for j in range(i, n):
+            length = i + j + 1
+            if length % 2 == 1:
+                total += sum(arr[i:j + 1])
+    return total
+
+
+print(sum_odd_length_subarrays([1, 4, 2, 5, 3]))
